0e2e06a0d7da1947bc7f6a65be1749248bd83632
Alexander V. Tikhonov <avtikhon@tarantool.org>
2021-03-02 11:35:16 +0300

    Add results file

--- run_comb_st.rb
+++ run_comb_st.rb
@@ -4,23 +4,27 @@ require 'optparse'
 require 'colorize'
 
 options = OpenStruct.new
+options.tnt_path = '.'
 options.branch = 'master'
 options.verbosity = 0
 options.com_tag = 0
 options.com_back = 0
+options.file_issues = "issues.txt"
 
 OptionParser.new do |opt|
+  opt.on("-s", "--source <TARANTOOL SOURCE PATH, default '#{options.tnt_path}'>") { |s| options.tnt_path = s }
   opt.on("-b", "--branch <BRANCH, ex. 1.10>") { |b| options.branch = b }
   opt.on("-t", "--tag <COMMIT TAG long format> (disables --commits_back option)") { |t| options.com_tag = t }
   opt.on("--commits_back <number of commits like for BRANCH~2, ex. 2>") { |c| options.com_back = c }
   opt.on("-v", "--verbosity <NUMBER, [0-3]>") { |v| options.verbosity = v.to_i }
+  opt.on("-f", "--file_issues <FILE NAME, default '#{options.file_issues}'>") { |f| options.file_issues = f }
 end.parse!
 
 if options.com_tag != 0
   git_sha = options.com_tag
 else
   # remove EOL by strip
-  git_sha = %x( git rev-parse origin/#{options.branch}~#{options.com_back} ).strip
+  git_sha = %x( cd #{options.tnt_path} && git rev-parse origin/#{options.branch}~#{options.com_back} ).strip
 end
 if options.verbosity > 0
   puts "Options: #{options}"
@@ -41,6 +45,8 @@ if options.verbosity > 1
   puts "There are #{last_response.data.total_count} results, on #{number_of_pages} pages!"
 end
 
+file = File.open(options.file_issues, 'w')
+
 # print in loop all paginated pages
 loop do
   # for verbosity print all the current page data
@@ -55,7 +61,9 @@ loop do
       if item[:conclusion] == 'success'
         puts "#{item[:conclusion].green} #{item[:name]}"
       else
-        puts "#{item[:conclusion].red} #{item[:name]}"
+        issue = "#{item[:conclusion].red} #{item[:name]}"
+        file.write("#{issue}\n")
+        puts issue
       end
     end
   end
@@ -68,3 +76,4 @@ loop do
   last_response = last_response.rels[:next].get
 end
 
+file.close()
